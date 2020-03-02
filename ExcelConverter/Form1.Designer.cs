814f46bfc570ab17c37ceb9cd36c4d870151ac46
Tam Ngo <minhtam0412@gmail.com>
2020-03-02 16:51:52 +0700

    thêm mới code

---
+++ ExcelConverter/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ExcelConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chkReplaceNewLine = new System.Windows.Forms.CheckBox();
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExportResult = new System.Windows.Forms.Button();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(536, 10);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Xử lý";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(419, 379);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(102, 38);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(491, 388);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 62);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer1.Size = new System.Drawing.Size(1029, 388);
+            this.splitContainer1.SplitterDistance = 491;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(534, 388);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chkReplaceNewLine);
+            this.panel1.Controls.Add(this.txtSourcePath);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnExportResult);
+            this.panel1.Controls.Add(this.btnChooseFile);
+            this.panel1.Controls.Add(this.btnProcess);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1029, 62);
+            this.panel1.TabIndex = 4;
+            // 
+            // chkReplaceNewLine
+            // 
+            this.chkReplaceNewLine.AutoSize = true;
+            this.chkReplaceNewLine.Location = new System.Drawing.Point(138, 38);
+            this.chkReplaceNewLine.Name = "chkReplaceNewLine";
+            this.chkReplaceNewLine.Size = new System.Drawing.Size(193, 17);
+            this.chkReplaceNewLine.TabIndex = 3;
+            this.chkReplaceNewLine.Text = "Tự động thay thế ký tự xuống dòng";
+            this.chkReplaceNewLine.UseVisualStyleBackColor = true;
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.Location = new System.Drawing.Point(138, 12);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.ReadOnly = true;
+            this.txtSourcePath.Size = new System.Drawing.Size(277, 20);
+            this.txtSourcePath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đường dẫn tập tin gốc";
+            // 
+            // btnExportResult
+            // 
+            this.btnExportResult.Location = new System.Drawing.Point(627, 10);
+            this.btnExportResult.Name = "btnExportResult";
+            this.btnExportResult.Size = new System.Drawing.Size(132, 23);
+            this.btnExportResult.TabIndex = 0;
+            this.btnExportResult.Text = "Xuất kết quả ra excel";
+            this.btnExportResult.UseVisualStyleBackColor = true;
+            this.btnExportResult.Click += new System.EventHandler(this.btnExportResult_Click);
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(445, 10);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Chọn file";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Công cụ chuyển đổi font";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnExportResult;
+        private System.Windows.Forms.CheckBox chkReplaceNewLine;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ExcelConverter/Form1.cs
@@ -0,0 +1,128 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Forms;
+
+namespace ExcelConverter
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class Form1 : Form
+    {
+        //https://vietcatholicsydney.net/Convert/index.htm
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable dtbDestination;
+
+        public DataTable ReadExcelFile(string filePath)
+        {
+            DataTable dtexcel = new DataTable();
+            bool hasHeaders = false;
+            string HDR = hasHeaders ? "Yes" : "No";
+            string strConn;
+            if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + HDR + ";IMEX=0\"";
+            else
+                strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=" + HDR + ";IMEX=0\"";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            //Looping a first Sheet of Xl File
+            DataRow schemaRow = schemaTable.Rows[0];
+            string sheet = schemaRow["TABLE_NAME"].ToString();
+            if (!sheet.EndsWith("_"))
+            {
+                string query = "SELECT  * FROM [" + sheet + "]";
+                OleDbDataAdapter daexcel = new OleDbDataAdapter(query, conn);
+                dtexcel.Locale = CultureInfo.CurrentCulture;
+                daexcel.Fill(dtexcel);
+            }
+
+            conn.Close();
+            return dtexcel;
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSourcePath.Text))
+            {
+                MessageBox.Show("Vui lòng chọn tập tin cần chuyển đổi!");
+                return;
+            }
+            var dtbSource = this.ReadExcelFile(txtSourcePath.Text.Trim());
+            dataGridView1.DataSource = dtbSource;
+            dtbDestination = dtbSource.Copy();
+            foreach (DataRow row in dtbDestination.Rows)
+            {
+                foreach (DataColumn column in dtbDestination.Columns)
+                {
+                    var src = Convert.ToString(row[column]);
+                    if (!string.IsNullOrEmpty(src))
+                    {
+                        {
+                            var rsl = webBrowser1.Document.InvokeScript("Convert", new object[] { src, 3, 5 });
+                            Debug.WriteLine(src + " -> " + rsl);
+
+                            row[column] = chkReplaceNewLine.Checked ? rsl.ToString().Replace("\n", " ").Replace("\r\n", " ") : rsl;
+                        }
+                    }
+                }
+            }
+
+            dataGridView2.DataSource = dtbDestination;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.ObjectForScripting = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            webBrowser1.Navigate(Path.Combine(currentPath, "HTMLPage1.html"));
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void btnExportResult_Click(object sender, EventArgs e)
+        {
+            if (dtbDestination == null || dtbDestination.Rows.Count == 0)
+            {
+                MessageBox.Show("Không có kết quả để xuất!");
+                return;
+            }
+
+            saveFileDialog1.Title = "Lưu kết quả";
+            saveFileDialog1.Filter = "Excel 2007 above|*.xlsx|Excel 2003|*.xls";
+            saveFileDialog1.DefaultExt = "xlsx";
+            saveFileDialog1.CheckPathExists = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog1.FileName;
+                XLWorkbook xlWorkbook = new XLWorkbook();
+                xlWorkbook.Worksheets.Add(dtbDestination, "Sheet1");
+                xlWorkbook.SaveAs(fileName);
+            }
+        }
+
+        private void btnChooseFile_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Excel 2007 above|*.xlsx|Excel 2003|*.xls";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Title = "Chọn tập tin";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSourcePath.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
